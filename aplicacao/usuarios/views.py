d5b28adf7acdc6a95385c796a36835f085d7a86d
Gabriel Pessighello <gabriel.pessighello@indico.net.br>
2020-11-18 20:45:13 -0300

    - Enviado submissao do formulario via POST - Utilizando variavel csrf do django para evitar problemas de cross-origin - Validando envio dos dados de cadastro

--- aplicacao/usuarios/views.py
+++ aplicacao/usuarios/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 
 def cadastro(request):
-    return render(request, 'usuarios/cadastro.html')
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['pasword']
+        senha2 = request.POST['pasword2']
+
+        if not nome.strip():
+            print('O campo nome deve ser corretamente preenchido')
+            return redirect('cadastro')
+
+        if not email.strip():
+            print('O campo email deve ser corretamente preenchido')
+            return redirect('cadastro')
+
+        if senha != senha2:
+            print('As senhas não são iguais')
+
+        if User.objects.filter(email=email).exists():
+            print('Usuário com este e-mail já existe')
+            return redirect('cadastro')
+
+        user = User.objects.create_user(username=nome, email=email, password=senha)
+        user.save()
+
+        print(f'Dados do cadastro\nNome: {nome}\nE-mail: {email}\nSenha: {senha}')
+        return redirect('login')
+    else:
+        return render(request, 'usuarios/cadastro.html')
 
 def login(request):
     return render(request, 'usuarios/login.html')
