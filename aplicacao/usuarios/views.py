927f66867e15f64df069eac1d5fc899d7c59ce25
GabrielNP <gabrielnp.2010@gmail.com>
2020-11-19 21:55:27 -0300

    Refactoring reusable functions to validate sign in and sign up

--- aplicacao/usuarios/views.py
+++ aplicacao/usuarios/views.py
@@ -10,27 +10,29 @@ def cadastro(request):
         senha = request.POST['password']
         senha2 = request.POST['password2']
 
-        if not nome.strip():
-            print('O campo nome deve ser corretamente preenchido')
+        if campo_vazio(nome):
+            messages.error(request, 'O campo nome deve ser corretamente preenchido')
             return redirect('cadastro')
 
-        if not email.strip():
-            print('O campo email deve ser corretamente preenchido')
+        if campo_vazio(email):
+            messages.error(request, 'O campo email deve ser corretamente preenchido')
             return redirect('cadastro')
 
-        if senha != senha2:
+        if senhas_nao_sao_iguais(senha, senha2):
             messages.error(request, 'As senhas não são iguais')
-            print('As senhas não são iguais')
             return redirect('cadastro')
 
         if User.objects.filter(email=email).exists():
-            print('Usuário com este e-mail já existe')
+            messages.error(request, 'Usuário com este e-mail já existe')
+            return redirect('cadastro')
+
+        if User.objects.filter(username=nome).exists():
+            messages.error(request,'Usuário já cadastrado')
             return redirect('cadastro')
 
         user = User.objects.create_user(username=nome, email=email, password=senha)
         user.save()
 
-        print(f'Dados do cadastro\nNome: {nome}\nE-mail: {email}\nSenha: {senha}\nSenha2: {senha2}')
         messages.success(request, 'Cadastro realizado com sucesso')
         return redirect('login')
     else:
@@ -75,10 +77,10 @@ def dashboard(request):
 def login(request):
     if request.method == 'POST':
         email = request.POST['email']
-        senha = request.POST['pasword']
+        senha = request.POST['senha']
 
-        if email == "" or senha == "":
-            print('Os campos email e senha não podem ficar em branco')
+        if campo_vazio(email) or campo_vazio(senha):
+            messages.error(request, 'Os campos e-mail e senha não podem ficar em branco')
             return redirect('login')
 
         if User.objects.filter(email=email).exists():
@@ -95,3 +97,9 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return redirect('login')
+
+def campo_vazio(campo):
+    return not campo.strip()
+
+def senhas_nao_sao_iguais(senha1, senha2):
+    return senha1 != senha2
