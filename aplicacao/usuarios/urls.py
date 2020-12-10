d7afc7fb0c57fe3788b2ca0311d24c4965a8cd29
GabrielNP <gabrielnp.2010@gmail.com>
2020-12-09 21:56:44 -0300

    Views refactored

--- aplicacao/usuarios/urls.py
+++ aplicacao/usuarios/urls.py
@@ -2,12 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('atualiza_receita', views.atualiza_receita, name='atualiza_receita'),
     path('cadastro', views.cadastro, name='cadastro'),
-    path('cria/receita', views.cria_receita, name='cria_receita'),
     path('dashboard', views.dashboard, name='dashboard'),
-    path('deleta/<int:receita_id>', views.deleta_receita, name='deleta_receita'),
-    path('edita/<int:receita_id>', views.edita_receita, name='edita_receita'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout')
 ]
